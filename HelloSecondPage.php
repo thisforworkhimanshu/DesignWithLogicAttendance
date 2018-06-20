22ef41e24936de4e95db6638fee872fce158b49e
Himanshu <Himanshu@DESKTOP-FPAR8AJ>
2018-06-20 17:16:47 +0530

    Project SGHAM Hello Page

--- HelloSecondPage.php
+++ HelloSecondPage.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo 'Hello Second';
+            echo 'Hello Second Page';
         ?>
     </body>
 </html>
